7d49ed99fafbc8da65aee35ffa3d9d36b55a859e
Bradley Haveman <bradley@haveman.ca>
2023-03-18 14:50:59 -0400

    Fetching data

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,8 +1,8 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
-  const { data } = await supabase.from("book_info").select();
+  const { data } = await supabase.from("book_info").select().ilike('title_short', '%genesis%')
   return {
 	books: data ?? [],
-  };
+  }
 }
